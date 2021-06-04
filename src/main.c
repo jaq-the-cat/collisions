068bb55d20e137da8c8ae389e9e407bfce3f9a70
jaq-cat <jaq.cat@protonmail.ch>
2021-06-04 10:56:33 -0300

    first object is now movable :D

--- src/main.c
+++ src/main.c
@@ -4,10 +4,18 @@
 
 #define WIDTH 800
 #define HEIGHT 600
-#define BG 120
+#define BG 15
+
+#define SPD 10
 
 SDL_Window *win;
 SDL_Renderer *rend;
+
+typedef struct keyboard {
+    int w, a, s, d;
+} keyboard;
+
+keyboard keys = {0, 0, 0, 0};
 
 void init() {
     // Window and renderer
@@ -26,12 +34,35 @@ void init() {
     inittextures(rend);
 }
 
+int ud(SDL_EventType t) {
+    switch (t) {
+        case SDL_KEYDOWN:
+            return 1;
+        case SDL_KEYUP:
+            return 0;
+        default:
+            return 0;
+    }
+}
+
 int handleev() {
     SDL_Event event;
     SDL_PollEvent(&event);
     switch (event.key.keysym.scancode) {
         case SDL_SCANCODE_Q:
             return 1;
+        case SDL_SCANCODE_W:
+            keys.w = event.key.type == SDL_KEYDOWN;
+            break;
+        case SDL_SCANCODE_A:
+            keys.a = event.key.type == SDL_KEYDOWN;
+            break;
+        case SDL_SCANCODE_S:
+            keys.s = event.key.type == SDL_KEYDOWN;
+            break;
+        case SDL_SCANCODE_D:
+            keys.d = event.key.type == SDL_KEYDOWN;
+            break;
         default: break;
     }
      return 0;
@@ -51,13 +82,24 @@ int main() {
 
     // add objects
     addobj(createobj(10, 10, RED));
-    addobj(createobj(50, 20, GREEN));
-    addobj(createobj(60, 30, BLUE));
+    addobj(createobj(WIDTH/2-OW/2, HEIGHT/2-OH*3, GREEN));
+    addobj(createobj(WIDTH/2-OW/2, HEIGHT/2-OH/3, BLUE));
+
+    float yv = 0, xv = 0;
 
     int close = 0;
     while (!close) {
         printf("\e[1;1H\e[2J");
         close = handleev();
+
+        xv = -SPD * keys.a;
+        xv += SPD * keys.d;
+
+        yv = -SPD * keys.w;
+        yv += SPD * keys.s;
+
+        objs.arr[0].x += xv;
+        objs.arr[0].y += yv;
 
         SDL_SetRenderDrawColor(rend, BG, BG, BG, 255);
         SDL_RenderClear(rend);
