b17b5b9683da085779a5b788eebc133b7c30a7f6
jaq-cat <jaq.cat@protonmail.ch>
2021-06-27 16:47:53 -0300

    remove from quadtree

--- src/col.c
+++ src/col.c
@@ -1,6 +1,23 @@
 #include "col.h"
 #include <stdlib.h>
 
+void recursive_quadrants(quadtree *t, vec2 *point, void (*func)(quadtree*, vec2*)) {
+    s_quadrants *q = &t->data.quadrants;
+    const double mid_x = ((double) t->boundary.bl.x + (double) t->boundary.tr.x)/2.;
+    const double mid_y = ((double) t->boundary.bl.y + (double) t->boundary.tr.y)/2.;
+    if (point->x <= mid_x) { // west
+        if (point->y <= mid_y) // north
+            func(t->data.quadrants.nw, point);
+        else // south
+            func(t->data.quadrants.sw, point);
+    } else { // east
+        if (point->y <= mid_y) // north
+            func(t->data.quadrants.ne, point);
+        else // south
+            func(t->data.quadrants.se, point);
+    }
+}
+
 int point_inside_rect(vec2 *point, rectangle *rect) {
     return (
         point->x >= rect->bl.x &&
@@ -23,23 +40,32 @@ void insert(quadtree *t, vec2 *point) {
         t->data.points.length++;
         if (t->data.points.length == QT_CAPACITY)
             subdivide(t);
-    }
-    else {
-        s_quadrants *q = &t->data.quadrants;
-        const double mid_x = ((double) t->boundary.bl.x + (double) t->boundary.tr.x)/2.;
-        const double mid_y = ((double) t->boundary.bl.y + (double) t->boundary.tr.y)/2.;
-        if (point->x <= mid_x) { // west
-            if (point->y <= mid_y) // north
-                insert(t->data.quadrants.nw, point);
-            else // south
-                insert(t->data.quadrants.sw, point);
-        } else { // east
-            if (point->y <= mid_y) // north
-                insert(t->data.quadrants.ne, point);
-            else // south
-                insert(t->data.quadrants.se, point);
+    } else recursive_quadrants(t, point, insert);
+}
+
+void remove(quadtree *t, vec2 *point) {
+    if (t->type == POINTS) {
+        int i;
+
+        // Remove item
+        for (i=0; i<t->data.points.length; i++)
+            if (t->data.points.points[i] == point) {
+                t->data.points.points[i] = NULL;
+                break;
+            }
+        // Removed item's index is `i`
+
+        // For each index ahead of `i`, set `i` to that index and increase both
+        // This shifts the right part of the list left
+        // { 1 2 3 4 5 6 }
+        // { 1 2 3 5 6 NULL }
+        for (int k=i+1; k<t->data.points.length; k++, i++) {
+            t->data.points.points[i] = t->data.points.points[k];
+            t->data.points.points[k] = NULL;
         }
-    }
+        t->data.points.length--;
+
+    } else recursive_quadrants(t, point, remove);
 }
 
 vec2* closest_to(quadtree *t, vec2 *point) {
